0c5fa17c8b51b27881665dc3912cc6f816f82bd9
Matías Vergara <mavergaravicencio@estudiantes.unsam.edu.ar>
2025-12-14 17:57:17 -0300

    feat: seleccionar sucursal_id de tablas maestro y stock

--- sql/datamarts/dm_stock.sql
+++ sql/datamarts/dm_stock.sql
@@ -5,33 +5,17 @@
 
 CREATE OR REPLACE VIEW `{{ project_id }}.datamarts.dm_stock` AS
 SELECT
-  -- =========================
-  -- Métrica
-  -- =========================
   fs.stock,
-
-  -- =========================
-  -- Dimensión Tiempo
-  -- =========================
   df.fecha,
   df.anio,
   df.mes,
   df.semana_iso,
-
-  -- =========================
-  -- Dimensiones de Negocio
-  -- =========================
   dp.producto,
-  ds.distribuidor,
-  ds.provincia
-
+  ds.distribuidor
 FROM `{{ project_id }}.dwh.fact_stock` fs
-
 JOIN `{{ project_id }}.dwh.dim_fecha` df
   ON fs.fecha = df.fecha
-
 JOIN `{{ project_id }}.dwh.dim_producto` dp
   ON fs.producto_id = dp.producto_id
-
 JOIN `{{ project_id }}.dwh.dim_sucursal` ds
   ON fs.sucursal_id = ds.sucursal_id;
