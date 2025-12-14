78e4a9af506efb8125bca059b2cf01f7a558f0a4
Matías Vergara <mavergaravicencio@estudiantes.unsam.edu.ar>
2025-12-14 17:56:36 -0300

    feat: agregar coordenadas a datamart de ventas

--- sql/datamarts/dm_ventas.sql
+++ sql/datamarts/dm_ventas.sql
@@ -5,38 +5,23 @@
 
 CREATE OR REPLACE VIEW `{{ project_id }}.datamarts.dm_ventas` AS
 SELECT
-  -- =========================
-  -- Métricas
-  -- =========================
   fv.venta_unidades,
   fv.venta_importe,
-
-  -- =========================
-  -- Dimensión Tiempo
-  -- =========================
   df.fecha,
   df.anio,
   df.mes,
   df.semana_iso,
-
-  -- =========================
-  -- Dimensiones de Negocio
-  -- =========================
   dp.producto,
-  ds.provincia,
+  dc.provincia,
+  CONCAT(CAST(dc.coordenada_latitud AS STRING), ',', CAST(dc.coordenada_longitud AS STRING)) AS coordenadas,
   dc.tipo_negocio,
-  ds.distribuidor
-
+  dc.sucursal
 FROM `{{ project_id }}.dwh.fact_ventas` fv
-
 JOIN `{{ project_id }}.dwh.dim_fecha` df
   ON fv.fecha = df.fecha
-
 JOIN `{{ project_id }}.dwh.dim_cliente` dc
   ON fv.cliente_id = dc.cliente_id
-
 JOIN `{{ project_id }}.dwh.dim_producto` dp
   ON fv.producto_id = dp.producto_id
-
 JOIN `{{ project_id }}.dwh.dim_sucursal` ds
   ON fv.sucursal_id = ds.sucursal_id;
